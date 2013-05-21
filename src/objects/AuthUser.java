e0f054212d0e6ec00afc40153edcb3ca07f6bbc9
Marc Bernstein <github@marcanderica.org>
2013-05-20 22:56:28 -0700

    Remove comments

--- src/objects/AuthUser.java
+++ src/objects/AuthUser.java
@@ -3,18 +3,6 @@ package objects;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
-// <GoodreadsResponse>
-// <Request>
-// <authentication>true</authentication>
-// <key><![CDATA[84uSfOCs4L6R8VdgnDrOLQ]]></key>
-// <method><![CDATA[api_auth_user]]></method>
-// </Request>
-// <user id="2448689">
-// <name>Marc Bernstein</name>
-// <link><![CDATA[https://www.goodreads.com/user/show/2448689-marc-bernstein?utm_medium=api]]></link>
-// </user>
-//
-// </GoodreadsResponse>
 @Root(name = "GoodreadsResponse", strict = false)
 public class AuthUser {
 
